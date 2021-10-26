dc44814b754fa1a993d6cb0ec2e208b93bbaeab6
Simon Bdy <Simonbdy@users.noreply.github.com>
2021-10-26 11:28:28 +0200

    Fixed renamed files getting-started to get-started in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ module.exports = {
         {
           type: 'doc',
           label: 'Documentation',
-          docId: 'get-started/getting-started',
+          docId: 'get-started/get-started',
           position: 'right',
         },
         { to: '/tutorials/tutorials', label: 'Tutorials', position: 'right' },
@@ -83,7 +83,7 @@ module.exports = {
           items: [
             {
               label: 'Documentation',
-              to: '/docs/get-started/getting-started',
+              to: '/docs/get-started/get-started',
             },
             {
               label: 'Contact us',
