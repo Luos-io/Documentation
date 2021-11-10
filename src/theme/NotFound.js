353564440a7dd85b7ec336770474929a94563d4b
Alexis Geron <alexisgeron@MacBook-Pro-de-Alexis.local>
2021-11-10 14:54:32 +0100

    clean file

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -5,17 +5,13 @@
  * LICENSE file in the root directory of this source tree.
  */
 import React from 'react';
-import Layout from '@theme/Layout';
-import Translate, { translate } from '@docusaurus/Translate';
 import { Redirect } from '@docusaurus/router';
 import { useLocation } from 'react-router-dom';
 
 function NotFound() {
   const location = useLocation();
-  console.log(location);
   if (location.pathname.includes('/pages/')) {
     let newPathName = location.pathname.replace('/pages/', '/docs/');
-    console.log(newPathName);
     return <Redirect to={newPathName} />;
   }
 
