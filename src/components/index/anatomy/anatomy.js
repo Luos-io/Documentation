5cf949d03eda6688f51518bb1299c0ae9a6eae52
Alexis Geron <alexis.geron@luos.io>
2022-04-15 09:33:56 +0200

    remove wip page

--- src/components/index/anatomy/anatomy.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styles from './anatomy.module.css';
-
-const Anatomy = (props) => {
-  return <div className={styles.container}>test</div>;
-};
-export default Anatomy;
--- src/components/index/header/header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styles from './header.module.css';
-
-const Header = (props) => {
-  return <div className={styles.container}>test</div>;
-};
-export default Header;
