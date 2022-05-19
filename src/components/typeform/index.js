985d9e10bf64e3f98e3000c9ba711308d7d2f2ce
Alexis Geron <alexis.geron@luos.io>
2022-05-19 18:32:54 +0200

    add prototype typeform

---
+++ src/components/typeform/index.js
@@ -0,0 +1,11 @@
+import { Widget } from '@typeform/embed-react';
+import React from 'react';
+import '@typeform/embed/build/css/widget.css';
+
+const Form = () => (
+  <div>
+    <Widget id="uAfY4YhI" style={{ height: '500px' }} />
+  </div>
+);
+
+export default Form;
