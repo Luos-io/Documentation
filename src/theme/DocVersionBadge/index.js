ebdebfc60b5e31a2272af28c78742e520f554ff6
Alexis Geron <alexis.geron@luos.io>
2022-09-13 15:37:51 +0200

    add ROS img & version badge

---
+++ src/theme/DocVersionBadge/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import clsx from 'clsx';
+import Translate from '@docusaurus/Translate';
+import { ThemeClassNames } from '@docusaurus/theme-common';
+import { useDocsVersion } from '@docusaurus/theme-common/internal';
+import Link from '@docusaurus/Link';
+import versionList from './version.json';
+export default function DocVersionBadge({ className }) {
+  const versionMetadata = useDocsVersion();
+  const currentVersion =
+    versionMetadata.version !== 'current'
+      ? versionList[versionMetadata.version]
+      : versionMetadata.label;
+  if (versionMetadata.badge) {
+    return (
+      <Link
+        href={
+          versionMetadata.version !== 'current'
+            ? 'https://github.com/Luos-io/luos_engine/releases/tag/' + currentVersion
+            : null
+        }
+      >
+        <span
+          className={clsx(
+            className,
+            ThemeClassNames.docs.docVersionBadge,
+            'badge badge--secondary',
+          )}
+        >
+          <Translate id="theme.docs.versionBadge.label" values={{ versionLabel: currentVersion }}>
+            {'Version: {versionLabel}'}
+          </Translate>
+        </span>
+      </Link>
+    );
+  }
+  return null;
+}
