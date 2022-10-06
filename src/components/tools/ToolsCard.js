b2e417fa7d478bc4064d84bd3f28dfe65de4f38a
alexgeron-luos <alexis.geron@luos.io>
2022-10-06 09:28:59 +0200

    add new tools homepage

---
+++ src/components/tools/ToolsCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import { Paper } from '@mui/material';
+import Moment from 'moment';
+
+import dataJson from './data.json';
+
+import styles from './index.module.css';
+
+const ToolsCard = () => {
+  const data = dataJson.tools;
+  return (
+    <Grid container spacing={2} mb={5}>
+      {data.map((x, y) => (
+        <Grid className={styles.cardContainer} key={y} item xs={12} md={4}>
+          <a
+            href={x.link}
+            target={x.target !== undefined ? '_blank' : ''}
+            className={styles.link}
+            style={{ textDecoration: 'none' }}
+          >
+            <Paper className={styles.card} elevation={1}>
+              <img src={x.img} style={{ borderRadius: '4px' }} alt={x.title} />
+              <h2 className={styles.cardTitle}>{x.title}</h2>
+
+              <p className={styles.cardDesc}>{x.desc}</p>
+            </Paper>
+          </a>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default ToolsCard;
