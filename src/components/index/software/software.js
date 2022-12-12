9d1f8ad0683b572e02474578c52c0e5db798332d
Benjamin Christau <benjamin.christau@luos.io>
2022-12-12 18:55:57 +0100

    Update software bloc to to transform it in the "How luos engine works"

--- src/components/index/software/software.js
+++ src/components/index/software/software.js
@@ -45,229 +45,123 @@ const Software = (props) => {
       <h2 className={`${styles.title} ${styles.underline}`}>
         Develop scalable cyber-physical systems
       </h2>
-      <Grid container spacing={3} mb={5}>
-        <Grid item md={1.5} lg={1.5} xl={1.5}></Grid>
-        <Grid item md={10} lg={10}>
-          <Grid item md={1} lg={1}></Grid>
-          <Grid container spacing={3}>
-            <Grid item md={3} lg={3} className={styles.green}>
-              <h3 className={`${styles.subtitle} ${styles.underline}`}>
-                Use network agnostic data format
-              </h3>
-              <p style={{ textAlign: 'justify' }}>
-                Luos engine provides a network agnostic multimaster data format. No matter how
-                specific your network is.
-              </p>
+      <Grid container>
+        <Grid item md={1} lg={1} xl={1}></Grid>
+        <Grid item md={10} lg={10} xl={10} style={{ marginBottom: '-50px' }}>
+          <div className={styles.head}>
+            {' '}
+            <Grid container>
+              <Grid item xs={'auto'}>
+                {' '}
+                <img
+                  src="assets/images/index/header/buttons.svg"
+                  style={{ padding: '15px', width: '100px' }}
+                  alt="buttons"
+                />
+              </Grid>
+              <Grid item xs={true} style={{ textAlign: 'center' }}>
+                {' '}
+                <h2 className={`${styles.hiwTitle} ${styles.underline}`}>/How Luos engine works</h2>
+              </Grid>
             </Grid>
-            <Grid item md={1} lg={1}></Grid>
-            <Grid item md={3} lg={3} className={styles.purple}>
-              <h3 className={`${styles.subtitle} ${styles.underline}`}>
-                Develop portable and scalable code
-              </h3>
-              <p style={{ textAlign: 'justify' }}>
-                Luos engine allows you to create packages and make them portable and scalable. You
-                can share them with your team or the community.
-              </p>
+          </div>
+          <Grid container>
+            <Grid item md={1} lg={1} xl={1} className={styles.docs}>
+              <FileCopyIcon fontSize="large" className={styles.icons} />
+              <SearchIcon fontSize="large" className={styles.icons} />
+              {isDarkTheme ? (
+                <>
+                  {' '}
+                  <img src="assets/images/index/header/luos.svg" alt="luos" />{' '}
+                </>
+              ) : (
+                <>
+                  {' '}
+                  <img src="assets/images/index/header/luos-white.svg" alt="luos-white" />{' '}
+                </>
+              )}
             </Grid>
-            <Grid item md={1} lg={1}></Grid>
-            <Grid item md={3} lg={3} className={styles.orange}>
-              <h3 className={`${styles.subtitle} ${styles.underline}`}>
-                Encapsulate features into services
-              </h3>
-              <p style={{ textAlign: 'justify' }}>
-                Luos engine encapsulates embedded features in services with APIs. This allows you to
-                have direct access to the hardware.
-              </p>
+
+            <Grid item md={11} lg={11} className={styles.playerLeft}>
+              <Grid container>
+                <Grid item md={4} lg={4} xl={4} className={styles.step}>
+                  <div className={styles.file}>
+                    <div className={styles.fileName}>step_1.c</div>
+                  </div>
+                  <img
+                    src={
+                      isDarkTheme
+                        ? `assets/images/index/software/step1.svg`
+                        : `assets/images/index/software/step1-dark.svg`
+                    }
+                    alt="step one"
+                    className={styles.carousselImgDesktop}
+                    loading="lazy"
+                  />
+                  <h3 className={styles.carousselTitle}>1. ENCAPSULATION</h3>
+                  <p className={styles.carousselText}>
+                    Luos engine translates your embedded features into APIs.
+                  </p>
+                </Grid>
+                <Grid item md={4} lg={4} xl={4} className={styles.step}>
+                  <div className={styles.file}>
+                    <div className={styles.fileName}>step_2.py</div>
+                  </div>
+                  <img
+                    src={
+                      isDarkTheme
+                        ? `assets/images/index/software/step2.svg`
+                        : `assets/images/index/software/step2-dark.svg`
+                    }
+                    alt="step two"
+                    className={styles.carousselImgDesktop}
+                    loading="lazy"
+                  />
+                  <h3 className={styles.carousselTitle}>2. EXPOSITION</h3>
+                  <p className={styles.carousselText}>
+                    Luos makes these APIs accessible from anywhere.
+                  </p>
+                </Grid>
+                <Grid item md={4} lg={4} xl={4}>
+                  <div className={styles.file}>
+                    <div className={styles.fileName}>step_3.js</div>
+                  </div>
+                  <img
+                    src={
+                      isDarkTheme
+                        ? `assets/images/index/software/step3.svg`
+                        : `assets/images/index/software/step3-dark.svg`
+                    }
+                    alt="step three"
+                    className={styles.carousselImgDesktop}
+                    loading="lazy"
+                  />
+                  <h3 className={styles.carousselTitle}>3. EXPLOITATION</h3>
+                  <p className={styles.carousselText}>
+                    Luos provides you with integrations and tools to design the software
+                    architectures of your dreams.
+                  </p>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item md={12} lg={12} xl={12} className={styles.footer}>
+              {' '}
             </Grid>
           </Grid>
+        </Grid>
+        <Grid item md={1} lg={1} xl={1}></Grid>
+        <Grid item xs={12}>
+          <div className={styles.btnContainer}>
+            <Button
+              variant="contained"
+              className={styles.pinkBtn}
+              href="/blog/a-way-to-unleash-embedded-systems"
+            >
+              Read our blog posts
+            </Button>
+          </div>
         </Grid>
       </Grid>
-      <div className={styles.btnContainer}>
-        <Button
-          variant="contained"
-          className={styles.pinkBtn}
-          href="/blog/a-way-to-unleash-embedded-systems"
-        >
-          Read our blog posts
-        </Button>
-      </div>
-      <BrowserView>
-        <Grid container className={styles.mobileNone}>
-          <Grid item md={1} lg={1} xl={1}></Grid>
-          <Grid item md={10} lg={10} xl={10} style={{ marginBottom: '-50px' }}>
-            <div className={styles.head}>
-              {' '}
-              <Grid container>
-                <Grid item md={4} lg={4} xl={4}>
-                  {' '}
-                  <img
-                    src="assets/images/index/header/buttons.svg"
-                    style={{ padding: '15px', width: '100px' }}
-                    alt="buttons"
-                  />
-                </Grid>
-                <Grid item md={4} lg={4} xl={4} style={{ textAlign: 'center' }}>
-                  {' '}
-                  <h2 className={`${styles.hiwTitle} ${styles.underline}`}>/How it works</h2>
-                </Grid>
-                <Grid item md={4} lg={4} xl={4}></Grid>
-              </Grid>
-            </div>
-            <Grid container>
-              <Grid item md={1} lg={1} xl={1} className={styles.docs}>
-                <FileCopyIcon fontSize="large" className={styles.icons} />
-                <SearchIcon fontSize="large" className={styles.icons} />
-                {isDarkTheme ? (
-                  <>
-                    {' '}
-                    <img src="assets/images/index/header/luos.svg" alt="luos" />{' '}
-                  </>
-                ) : (
-                  <>
-                    {' '}
-                    <img src="assets/images/index/header/luos-white.svg" alt="luos-white" />{' '}
-                  </>
-                )}
-              </Grid>
-
-              <Grid item md={11} lg={11} className={styles.playerLeft}>
-                <Grid container>
-                  <Grid item md={4} lg={4} xl={4} className={styles.step}>
-                    <div className={styles.file}>
-                      <div className={styles.fileName}>step_1.c</div>
-                    </div>
-                    <img
-                      src={
-                        isDarkTheme
-                          ? `assets/images/index/software/step1.svg`
-                          : `assets/images/index/software/step1-dark.svg`
-                      }
-                      alt="step one"
-                      className={styles.carousselImgDesktop}
-                      loading="lazy"
-                    />
-                    <h3 className={styles.carousselTitle}>1. ENCAPSULATION</h3>
-                    <p className={styles.carousselText}>
-                      Luos engine translates your embedded features into APIs.
-                    </p>
-                  </Grid>
-                  <Grid item md={4} lg={4} xl={4} className={styles.step}>
-                    <div className={styles.file}>
-                      <div className={styles.fileName}>step_2.py</div>
-                    </div>
-                    <img
-                      src={
-                        isDarkTheme
-                          ? `assets/images/index/software/step2.svg`
-                          : `assets/images/index/software/step2-dark.svg`
-                      }
-                      alt="step two"
-                      className={styles.carousselImgDesktop}
-                      loading="lazy"
-                    />
-                    <h3 className={styles.carousselTitle}>2. EXPOSITION</h3>
-                    <p className={styles.carousselText}>
-                      Luos makes these APIs accessible from anywhere.
-                    </p>
-                  </Grid>
-                  <Grid item md={4} lg={4} xl={4}>
-                    <div className={styles.file}>
-                      <div className={styles.fileName}>step_3.js</div>
-                    </div>
-                    <img
-                      src={
-                        isDarkTheme
-                          ? `assets/images/index/software/step3.svg`
-                          : `assets/images/index/software/step3-dark.svg`
-                      }
-                      alt="step three"
-                      className={styles.carousselImgDesktop}
-                      loading="lazy"
-                    />
-                    <h3 className={styles.carousselTitle}>3. EXPLOITATION</h3>
-                    <p className={styles.carousselText}>
-                      Luos provides you with integrations and tools to design the software
-                      architectures of your dreams.
-                    </p>
-                  </Grid>
-                </Grid>
-              </Grid>
-              <Grid item md={12} lg={12} xl={12} className={styles.footer}>
-                {' '}
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item md={1} lg={1} xl={1}></Grid>
-        </Grid>
-      </BrowserView>
-      <MobileView>
-        <Grid containe className={styles.howitsworkMobile}>
-          <Grid item xs={12}>
-            <div className={styles.head}>
-              <Grid container>
-                <Grid item xs={4}>
-                  {' '}
-                  <img
-                    src="assets/images/index/header/buttons.svg"
-                    style={{ padding: '15px', width: '100px' }}
-                    alt="buttons"
-                  />
-                </Grid>
-                <Grid item xs={8} style={{}}>
-                  {' '}
-                  <h2
-                    className={`${styles.hiwTitle} ${styles.underline}`}
-                    style={{ marginLeft: '10px' }}
-                  >
-                    /How its work
-                  </h2>
-                </Grid>
-              </Grid>
-            </div>
-            <Grid container>
-              <Grid item xs={2} className={styles.docs}>
-                <FileCopyIcon fontSize="large" className={styles.icons} />
-                <SearchIcon fontSize="large" className={styles.icons} />
-                {isDarkTheme ? (
-                  <>
-                    {' '}
-                    <img src="assets/images/index/header/luos.svg" alt="luos" />{' '}
-                  </>
-                ) : (
-                  <>
-                    {' '}
-                    <img src="assets/images/index/header/luos-white.svg" alt="luos-white" />{' '}
-                  </>
-                )}
-              </Grid>
-
-              <Grid item xs={10} className={styles.player}>
-                <Carousel
-                  IndicatorIcon={anArrayOfNumbers}
-                  indicatorIconButtonProps={{
-                    style: {
-                      padding: '10px', // 1
-                      filter: 'brightness(50%)',
-                    },
-                  }}
-                  activeIndicatorIconButtonProps={{
-                    style: {
-                      filter: 'brightness(100%)',
-                    },
-                  }}
-                >
-                  {items.map((item, i) => (
-                    <Item key={i} item={item} />
-                  ))}
-                </Carousel>
-              </Grid>
-            </Grid>
-            <Grid item xs={12} className={styles.footer}>
-              {' '}
-            </Grid>
-          </Grid>
-        </Grid>
-      </MobileView>
     </div>
   );
 };
